a3fdcde67db23d5da539aa9ce1a7e3256c54cffc
David Avery <eismausen@gmail.com>
2021-11-24 10:06:06 -0800

    commit tree 441ba41d24f1b01afc77e641df12961359810ea7

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
+    def name=(input)
+        @name = input
+    end
+    def name
+        @name
+    end
 
+    def job=(input)
+        @job = input
+    end
+
+    def job
+        @job
+    end
 end
